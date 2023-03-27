29b53d18f452e0854536e7135c215512f7432850
NatePillay <nathan94pillay@gmail.com>
2023-03-27 12:16:25 -0700

    new commit

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-#create a file in /tmp
+# create a file in /tmp.
 file { 'school':
- content => 'I love Puppet',
- group => 'www-data',
- mode => '0744',
- owner => 'www-data',
- path => '/tmp/scool',
+  content =>  'I love Puppet',
+  group   =>  'www-data',
+  mode    => '0744',
+  owner   => 'www-data',
+  path    => '/tmp/school',
 }
