e110f02915a1bb93633661ef3c97d31900c4fe5d
NatePillay <nathan94pillay@gmail.com>
2023-04-24 20:18:42 +0000

    new commit

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-'''export the data to a CSV file + format'''
+'''export the data to a CSV file and format the data'''
 from sys import argv
 from requests import get
 import csv
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 	endpoint = 'https://jsonplaceholder.typicode.com'
 	usr = get(endpoint + '/users/' + argv[1]).json()['username']
 	todos = get(endpoint + '/todos?userId=' + argv[1]).json()
@@ -12,5 +12,5 @@ if __name__ == "__main__":
 
 	with open('{}.csv'.format(argv[1]), 'w') as file:
 		writer = csv.writer(file, quoting=csv.QUOTE_ALL)
-	  	for todo in todos:
+		for todo in todos:
 			writer.writerow([argv[1], usr, todo['completed'], todo['title']])
